538e7a5d587ff86bdb35adffc39c76cc34752de6
Samodya <samodya0203samadini@gmail.com>
2025-06-17 20:09:03 +0530

    Add files via upload

---
+++ Practicals/Graphs/Codes/graph10.m
@@ -0,0 +1,12 @@
+% Define nodes
+nodes = {'A','B','C','D','E','F','G'};
+
+% Define edges using source and target nodes
+s = [1 1 2 2 3 3]; %from A->B, A->C, B->D, B->E
+t = [2 3 4 5 6 7]; %to
+
+%create a directed graph
+G = digraph(s,t,[],nodes);
+
+plot(G,'Layout','layered','Direction','down');
+title('Simple tree with 3 levels');
---
+++ Practicals/Graphs/Codes/graph11.m
@@ -0,0 +1,17 @@
+% Define nodes
+nodes = {'A','B','C','D','E','F','G'};
+
+% Define edges using source and target nodes
+s = [1 1 2 2 3 3]; %from A->B, A->C, B->D, B->E
+t = [2 3 4 5 6 7]; %to
+
+%create a directed graph
+G = digraph(s,t,[],nodes);
+
+plot(G,'Layout','layered','Direction','down');
+title('Simple tree with 3 levels');
+
+weights = [5 3 2 4 6 1];
+G=digraph(s,t,weights,nodes);
+plot(G,'Layout','layered','Direction','down','EdgeLabel',G.Edges.Weight);
+title('Weighted tree with 3 levels');
